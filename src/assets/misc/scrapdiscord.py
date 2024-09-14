89e91ae35a899391dedbdb8e187c5a07e9923594
Frozenk <frozenk@frozenk.fr>
2024-09-14 21:32:28 +0200

    Update scrapdiscord.py

--- src/assets/misc/scrapdiscord.py
+++ src/assets/misc/scrapdiscord.py
@@ -1,27 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-import os
-import re
-
-# URL de la liste des serveurs Discord
-discord_urls_file = 'https://raw.githubusercontent.com/Frozenka/hacking-france/main/src/assets/misc/liste_discord.txt'
-
-# Définir le chemin du fichier de sortie
-output_file_path = os.path.join('src', 'assets', 'misc', 'discord_servers_info.json')
-
-# URL de l'image par défaut
-default_image_url = 'https://logo-marque.com/wp-content/uploads/2020/12/Discord-Logo.png'
-
-def fetch_discord_urls(url):
-    response = requests.get(url)
-    response.encoding = 'utf-8'  # Forcer l'encodage UTF-8
-    response.raise_for_status()
-    return response.text.splitlines()
-
-def is_valid_url(url):
-    return url.startswith(('http://', 'https://'))
-
 def extract_discord_info(url):
     if not is_valid_url(url):
         print(f"URL invalide : {url}")
@@ -38,9 +14,9 @@ def extract_discord_info(url):
     # Supprimer le texte à partir de "- discute avec"
     description = re.sub(r'-\s*discute\s*avec.*$', '', description).strip()
 
-    # Ne pas afficher le texte après " | " dans la description
-    if ' | ' in description:
-        description = description.split(' | ')[0].strip()
+    # Si le caractère "|" est présent, supprimer tout ce qui suit
+    if "|" in description:
+        description = description.split('|')[0].strip()
 
     # Recherche du nombre de membres dans la description
     members_match = re.search(r'Discord\s*(.*?)\s*autres\s*membres\s*et\s*profite\s*du\s*chat\s*vocal\s*et\s*textuel\s*gratuit', description, re.IGNORECASE)
@@ -71,22 +47,3 @@ def extract_discord_info(url):
         'image': image_url.strip(),
         'link': url.strip()
     }
-
-def main():
-    discord_urls = fetch_discord_urls(discord_urls_file)
-    discord_channels = []
-    
-    for url in discord_urls:
-        info = extract_discord_info(url)
-        if info is not None:  # Inclure seulement si info n'est pas None
-            discord_channels.append(info)
-            print(f"Infos récupérées pour {url}: {info}")
-
-    # Écriture des résultats dans un fichier JSON
-    with open(output_file_path, 'w', encoding='utf-8') as json_file:
-        json.dump(discord_channels, json_file, indent=2, ensure_ascii=False)
-
-    print(f"Les informations des serveurs Discord ont été sauvegardées dans '{output_file_path}'.")
-
-if __name__ == "__main__":
-    main()
